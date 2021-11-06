9867559347894adf87e7936333d46e57d8d611aa
Anthony Chen <anthonychen33@gmail.com>
2021-11-06 01:33:43 -0400

    Proper format for score table.

--- script.js
+++ script.js
@@ -1,55 +1,98 @@
 const formContainer = document.querySelector(".form-container");
 const addPlayerButton = document.querySelector(".add-button");
 const tableContainer = document.querySelector(".table-container");
+const headerContainer = document.querySelector(".headers-container");
 
 function addToTable(playerName) {
-    console.log(playerName);
+   
     const trElement = document.createElement("tr"); 
     const tdName = document.createElement("td"); 
     const tdContainer = document.createElement("td"); 
     tdContainer.classList.add("td-Container"); 
 
+    // POINTS //
+    const divPointsContainer = document.createElement("div"); 
+    divPointsContainer.classList.add("div-Points"); 
+
     const addOnePoint = document.createElement("button"); 
     addOnePoint.textContent = "+1"; 
     const subtractOnePoint = document.createElement("button"); 
     subtractOnePoint.textContent = "-1"; 
-
+    
     const addTwoPoint = document.createElement("button"); 
     addTwoPoint.textContent = "+2"; 
     const subtractTwoPoint = document.createElement("button"); 
     subtractTwoPoint.textContent = "-2"; 
-
+    
     const addThreePoint = document.createElement("button"); 
     addThreePoint.textContent = "+3"; 
     const subtractThreePoint = document.createElement("button"); 
     subtractThreePoint.textContent = "-3"; 
+     // POINTS //
+     // REBOUNDS //
+    const divReboundContainer = document.createElement("div"); 
+    divReboundContainer.classList.add("div-RB"); 
+    const reboundContainer = document.createElement("td"); 
+    reboundContainer.classList.add("rebound-Container");
 
+    const addOneRebound = document.createElement("button"); 
+    addOneRebound.textContent = "+1"; 
+    const subtractOneRebound = document.createElement("button"); 
+    subtractOneRebound.textContent = "-1"; 
+     // REBOUNDS //
+     // FIELD GOALS //
+    const fieldGoalContainer = document.createElement("td"); 
+    fieldGoalContainer.classList.add("fieldGoal-Container");
+
+    const madeFG = document.createElement("button"); 
+    madeFG.textContent = "+1"; 
+    const missedFG = document.createElement("button"); 
+    missedFG.textContent = "-1"; 
+    // FIELD GOALS //
     
-    let tdPoints = document.createElement("td");
+    let tdPoints = document.createElement("div");
+    let tdRebounds = document.createElement("div"); 
+    let tdFieldGoals = document.createElement("div"); 
 
     if ( playerName.localeCompare("") === 0 ) {
         return 0; 
     } else {
         tdName.textContent = playerName; 
         tdPoints.textContent = "0"; 
-        tdContainer.appendChild(subtractThreePoint); 
-        tdContainer.appendChild(subtractTwoPoint); 
-        tdContainer.appendChild(subtractOnePoint);
-        tdContainer.appendChild(tdPoints);  
-        tdContainer.appendChild(addOnePoint); 
-        tdContainer.appendChild(addTwoPoint); 
-        tdContainer.appendChild(addThreePoint);
+        tdRebounds.textContent = "0"; 
+        tdFieldGoals.textContent = "0-0";
 
+        
+        divPointsContainer.appendChild(subtractThreePoint); 
+        divPointsContainer.appendChild(subtractTwoPoint); 
+        divPointsContainer.appendChild(subtractOnePoint);
+        divPointsContainer.appendChild(tdPoints);  
+        divPointsContainer.appendChild(addOnePoint); 
+        divPointsContainer.appendChild(addTwoPoint); 
+        divPointsContainer.appendChild(addThreePoint);
+        tdContainer.appendChild(divPointsContainer); 
+        
+        divReboundContainer.appendChild(subtractOneRebound);
+        divReboundContainer.appendChild(tdRebounds); 
+        divReboundContainer.appendChild(addOneRebound);
+        reboundContainer.appendChild(divReboundContainer); 
+        
+        fieldGoalContainer.appendChild(madeFG); 
+        fieldGoalContainer.appendChild(tdFieldGoals); 
+        fieldGoalContainer.appendChild(missedFG);
         
        // tdContainer.appendChild(tdPoints); 
         trElement.appendChild(tdName);
         trElement.appendChild(tdContainer);  
+        trElement.appendChild(reboundContainer); 
+        trElement.appendChild(fieldGoalContainer);
         tableContainer.appendChild(trElement); 
+        
         return 1; 
     }
 
     
-}
+} 
 
 function createForm() {
 
