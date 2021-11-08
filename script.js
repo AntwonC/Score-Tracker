cb59fd86fe67db4db61632318e40295a84b3a557
Anthony Chen <anthonychen33@gmail.com>
2021-11-08 15:56:02 -0500

    Added exporting data functionality. Completed.

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const formContainer = document.querySelector(".form-container");
 const addPlayerButton = document.querySelector(".add-button");
 const tableContainer = document.querySelector(".table-container");
 const headerContainer = document.querySelector(".headers-container");
+const exportData = document.querySelector(".export-data"); 
 
 function addToTable(playerName) {
    
     const trElement = document.createElement("tr"); 
+    // NAME //
     const tdName = document.createElement("td"); 
     const tdContainer = document.createElement("td"); 
     tdContainer.classList.add("td-Container"); 
+    // NAME //
 
     // POINTS //
     const divPointsContainer = document.createElement("div"); 
@@ -41,6 +44,8 @@ function addToTable(playerName) {
     subtractOneRebound.textContent = "-1"; 
      // REBOUNDS //
      // FIELD GOALS //
+    const divFGContainer = document.createElement("div");
+    divFGContainer.classList.add("div-FG");
     const fieldGoalContainer = document.createElement("td"); 
     fieldGoalContainer.classList.add("fieldGoal-Container");
 
@@ -50,19 +55,23 @@ function addToTable(playerName) {
     missedFG.textContent = "-1"; 
     // FIELD GOALS //
     
-    let tdPoints = document.createElement("div");
-    let tdRebounds = document.createElement("div"); 
-    let tdFieldGoals = document.createElement("div"); 
+    let pName = document.createElement("p");
+    let tdPoints = document.createElement("p");
+    let tdRebounds = document.createElement("p"); 
+    let tdFieldGoals = document.createElement("p"); 
 
     if ( playerName.localeCompare("") === 0 ) {
         return 0; 
     } else {
-        tdName.textContent = playerName; 
+        pName.textContent = playerName; 
         tdPoints.textContent = "0"; 
         tdRebounds.textContent = "0"; 
         tdFieldGoals.textContent = "0-0";
+        // NAME //
+        tdName.appendChild(pName);
+        // NAME //
 
-        
+        // POINTS //
         divPointsContainer.appendChild(subtractThreePoint); 
         divPointsContainer.appendChild(subtractTwoPoint); 
         divPointsContainer.appendChild(subtractOnePoint);
@@ -71,16 +80,20 @@ function addToTable(playerName) {
         divPointsContainer.appendChild(addTwoPoint); 
         divPointsContainer.appendChild(addThreePoint);
         tdContainer.appendChild(divPointsContainer); 
-        
+        // POINTS //
+        // REBOUNDS //
         divReboundContainer.appendChild(subtractOneRebound);
         divReboundContainer.appendChild(tdRebounds); 
         divReboundContainer.appendChild(addOneRebound);
         reboundContainer.appendChild(divReboundContainer); 
-        
-        fieldGoalContainer.appendChild(madeFG); 
-        fieldGoalContainer.appendChild(tdFieldGoals); 
-        fieldGoalContainer.appendChild(missedFG);
-        
+        // REBOUNDS //
+        // FIELD GOALS //
+        divFGContainer.appendChild(madeFG); 
+        divFGContainer.appendChild(tdFieldGoals); 
+        divFGContainer.appendChild(missedFG);
+        fieldGoalContainer.appendChild(divFGContainer);
+        // FIELD GOALS //
+
        // tdContainer.appendChild(tdPoints); 
         trElement.appendChild(tdName);
         trElement.appendChild(tdContainer);  
@@ -88,11 +101,155 @@ function addToTable(playerName) {
         trElement.appendChild(fieldGoalContainer);
         tableContainer.appendChild(trElement); 
         
+        //
+        addOnePoint.addEventListener("click", () => {
+           let resValue = changeNumber(tdPoints.textContent, 1); 
+           tdPoints.textContent = resValue;
+        }); 
+
+        subtractOnePoint.addEventListener("click", () => {
+            let resValue = changeNumber(tdPoints.textContent, -1); 
+            tdPoints.textContent = resValue;
+         }); 
+
+        addTwoPoint.addEventListener("click", () => {
+            let resValue = changeNumber(tdPoints.textContent, 2); 
+            tdPoints.textContent = resValue;
+        }); 
+
+        subtractTwoPoint.addEventListener("click", () => {
+            let resValue = changeNumber(tdPoints.textContent, -2); 
+            tdPoints.textContent = resValue;
+        }); 
+
+        addThreePoint.addEventListener("click", () => {
+            let resValue = changeNumber(tdPoints.textContent, 3); 
+            tdPoints.textContent = resValue;
+        }); 
+
+        subtractThreePoint.addEventListener("click", () => {
+            let resValue = changeNumber(tdPoints.textContent, -3); 
+            tdPoints.textContent = resValue;
+        }); 
+
+        addOneRebound.addEventListener("click", () => {
+            let resValue = changeNumber(tdRebounds.textContent, 1); 
+            tdRebounds.textContent = resValue;
+         });
+
+        subtractOneRebound.addEventListener("click", () => {
+            let resValue = changeNumber(tdRebounds.textContent, -1); 
+            tdRebounds.textContent = resValue;
+        });
+
+        // Field Goals 
+        madeFG.addEventListener("click", () => {
+            let resValue = updateFG(tdFieldGoals.textContent, true); 
+            tdFieldGoals.textContent = resValue; 
+        });
+
+        missedFG.addEventListener("click", () => {
+            let resValue = updateFG(tdFieldGoals.textContent, false); 
+            tdFieldGoals.textContent = resValue; 
+        });
         return 1; 
     }
 
     
 } 
+
+function changeNumber(number, value) {
+    let num = +number; 
+    num += value;
+    return num.toString();
+}
+
+function updateFG(score, result) {
+    let firstNumber = ""; 
+    let secondNumber = ""; 
+    let nextNumber = false; 
+
+    for(let i = 0; i < score.length; i++) {
+        if ( score.charAt(i) === '-' ) {
+            nextNumber = true;
+            continue;  
+        }
+
+        if ( nextNumber ) {
+            secondNumber += score.charAt(i); 
+        } else {
+            firstNumber += score.charAt(i); 
+        }
+    }
+
+    if ( result ) {    
+        firstNumber = +firstNumber + 1;
+        secondNumber = +secondNumber + 1;
+        let finalString = firstNumber + "-" + secondNumber; 
+        return finalString; 
+    } else {
+        secondNumber = +secondNumber + 1
+        let finalString = firstNumber + "-" + secondNumber; 
+        return finalString; 
+    }
+
+}
+
+function exportingToFile(tableData) {
+   
+    var textFile = null,
+    makeTextFile = function (text) {
+        var data = new Blob([text], {type: 'text/plain'});
+        
+        // If we are replacing a previously generated file we need to
+        // manually revoke the object URL to avoid memory leaks.
+        if (textFile !== null) {
+            window.URL.revokeObjectURL(textFile);
+        }
+        
+        textFile = window.URL.createObjectURL(data);
+        
+        // returns a URL you can use as a href
+        return textFile;
+    };
+    
+    let pData = tableData.getElementsByTagName('p'); 
+    let data = "[Name] | [Points] | [Rebounds] | [Field Goal]\n\n";
+    let count = 0; 
+
+    // Empty table, don't allow downloadable link
+    if ( pData.length === 0 || pData === undefined ) {
+        return -1; 
+    }
+
+    for(let i = 0; i < pData.length; i++) {
+
+        if ( count === 4 ) {
+            data += "\n\n";
+            count = 0; 
+        }
+        data += "[" + pData[i].textContent + "]" + "|";
+        console.log(`pData: ${pData[i].textContent}`);
+        count++; 
+        
+    }
+
+    let link = document.getElementById("downloadLink"); 
+    link.href = makeTextFile(data); 
+    link.style.display = "block";
+
+    setTimeout(() => {
+        link.style.display = "none"; 
+    }, 3000); 
+    /*for( let i in tableData.rows ) {
+        let row = tableData.rows[i]; 
+        console.log(`row: ${row.textContent}`);
+        for( let j in row.cells ) {
+            let col = row.cells[j];
+            //console.log(`col: ${col.textContent}`);
+        }
+    }  */
+}
 
 function createForm() {
 
@@ -126,3 +283,7 @@ function createForm() {
 }
 
 addPlayerButton.addEventListener("click", createForm); 
+
+exportData.addEventListener("click", () => {
+    exportingToFile(tableContainer);
+});
